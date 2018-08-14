7b9f53c579c973c2912e9adbe58599d99b40a288
dan jones <dannyjones360@gmail.com>
2018-08-14 14:55:54 +0100

    Minor amends

--- src/app/components/articles-latest/index.js
+++ src/app/components/articles-latest/index.js
@@ -4,16 +4,21 @@ import LazyImg from '../../elements/lazy-image'
 class ArticlesLatestComponent extends Component {
   render() {
     const { items } = this.props;
+    console.log(items);
 
     return (
-      <React.Fragment>
+      <div className="flex  flex-wrap">
         {items.map((item, i) => (
-          <div>
-            <LazyImg src="http://via.placeholder.com/350x150"  alt="" />
-            <p>{item.title}</p>
+          <div key={i} className="col-24  col-12-sm  col-6-md">
+            <figure>
+              <LazyImg src="http://via.placeholder.com/200x200" alt="" />
+              <figcaption>{item.title}</figcaption>
+            </figure>
+            <time pubdate datetime="2011-08-28" title="August 28th, 2011">{item._createdAt}</time>
+            <address>{item.author.name}</address>
           </div>
         ))}
-      </React.Fragment>
+      </div>
     );
   }
 }
--- src/app/elements/lazy-image/index.js
+++ src/app/elements/lazy-image/index.js
@@ -4,10 +4,10 @@ import LazyLoad from 'react-lazyload';
 class LazyImage extends Component {
   render() {
     const { src, alt } = this.props;
-    
+
     return (
       <React.Fragment>
-        <LazyLoad height={200} once>
+        <LazyLoad height={200} offset={100} once>
           <img src={src} alt={alt}/>
         </LazyLoad>
       </React.Fragment>
