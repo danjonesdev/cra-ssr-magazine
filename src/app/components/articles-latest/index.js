32c598932e6fe468efea6e124337a5d61f632b2d
dan jones <dannyjones360@gmail.com>
2018-08-14 17:18:55 +0100

    Further amends

--- src/app/components/articles-latest/index.js
+++ src/app/components/articles-latest/index.js
@@ -8,18 +8,22 @@ class ArticlesLatestComponent extends Component {
     console.log(items);
 
     return (
-      <section className={`flex  flex-wrap  ${padding}`}>
-        {items.map((item, i) => (
-          <article key={i} className="col-24  col-12-sm  col-6-md">
-            <figure>
-              <LazyImg src={item.mainImage} alt={item.title} />
-              <figcaption>{item.title}</figcaption>
-            </figure>
-            <time pubdate={item.publishedAt} dateTime={item.publishedAt} title={item.publishedAt}>{item.publishedAt}</time>
-            <address>{item.author.name}</address>
-            <p>{item.description}</p>
-          </article>
-        ))}
+      <section className={`container  mla  mra  ${padding}`}>
+        <div className="flex  flex-wrap">
+          {items.map((item, i) => (
+            <article key={i} className="col-24  col-12-sm  col-6-md  pa3">
+              <figure>
+                <LazyImg src={item.mainImage} alt={item.title} />
+                <figcaption className="t-title  bold  f6  f5-md  pv2">{item.title}</figcaption>
+              </figure>
+
+              <address className="t-body  f7  f6-md  dib pb2">{item.author.name}</address>
+              <span className="t-body  f7  f6-md  dib  ph2  pb2">|</span>
+              <time className="t-body  f7  f6-md  dib pb2" pubdate={item.publishedAt} dateTime={item.publishedAt} title={item.publishedAt}>{item.publishedAt}</time>
+              <p className="t-body  f7  f6-md">{item.description}</p>
+            </article>
+          ))}
+        </div>
       </section>
     );
   }
