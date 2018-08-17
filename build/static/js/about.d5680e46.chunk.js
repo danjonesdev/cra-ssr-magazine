315195ea7cfb0fedbf235b05075b69c2ef219324
dan jones <dannyjones360@gmail.com>
2018-08-17 23:53:36 +0100

    add buildserve script

--- build/static/js/about.d5680e46.chunk.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([4],{223:function(e,t,a){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var l=a(0),u=a.n(l),n=a(68);t.default=function(){return u.a.createElement(n.a,{id:"about",title:"About",description:"This is about really cool stuff."},u.a.createElement("p",null,"What we're all about"))}}});
-//# sourceMappingURL=about.d5680e46.chunk.js.map
