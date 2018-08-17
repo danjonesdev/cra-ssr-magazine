1d3988ea20e9968aed5e687353d3f319e49603af
dan jones <dannyjones360@gmail.com>
2018-08-17 23:47:59 +0100

    build

--- src/app/components/articles-carousel/index.js
+++ src/app/components/articles-carousel/index.js
@@ -1,19 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import Flickity from 'react-flickity-component'
 
 import AnimatedImage from '../../elements/animated-image'
 
 class ArticlesCarousel extends Component {
-
-  flickityOptions = {
-    wrapAround: true,
-    autoPlay: 2000,
-    pauseAutoPlayOnHover: false,
-    prevNextButtons: false,
-    cellAlign: 'center',
-    pageDots: false,
-  }
 
   render() {
     const { items } = this.props;
@@ -21,33 +11,25 @@ class ArticlesCarousel extends Component {
 
     return (
       <section className={`container-medium  mla  mra  ph0-sm  ${padding}`}>
-        <Flickity
-          className={'carousel'} // default ''
-          elementType={'div'} // default 'div'
-          options={this.flickityOptions} // takes flickity options {}
-          disableImagesLoaded={true} // default false
-          reloadOnUpdate={false} // default false
-        >
-          {items.map((item, i) => (
-            <article key={i} className="w-100  w-40-sm  mlra  mra  pa3">
-              <figure>
-                <Link className="link  db  shadow2" to={`/article/${item.slug.current}`}>
-                  <AnimatedImage lazy={false} src={item.mainImage} alt={item.title} styles="trans-all-1  h7  h9-sm  w-100" />
-                </Link>
-              </figure>
-
-              <div className="pt2  mt2  tac">
-                <address className="t-body  f6  mid-grey  dib">{item.author.name}</address>
-                <span className="t-body  f6  mid-grey  dib  ph1">|</span>
-                <time className="t-body  f6  mid-grey  dib" dateTime={item.publishedAt}>{item.publishedAt.split('T')[0]}</time>
-              </div>
-              <Link className="link" to={`/article/${item.slug.current}`}>
-                <p className="t-title  bold  f6  f5-md  black  tac  pt1  pb2">{item.title}</p>
+        {items.map((item, i) => (
+          <article key={i} className="w-100  w-40-sm  mlra  mra  pa3">
+            <figure>
+              <Link className="link  db  shadow2" to={`/article/${item.slug.current}`}>
+                <AnimatedImage lazy={false} src={item.mainImage} alt={item.title} styles="trans-all-1  h7  h9-sm  w-100" />
               </Link>
-              <p className="t-body  f6  mid-grey  tac  rel">{item.description.substring(0, 150)}...</p>
-            </article>
-          ))}
-        </Flickity>
+            </figure>
+
+            <div className="pt2  mt2  tac">
+              <address className="t-body  f6  mid-grey  dib">{item.author.name}</address>
+              <span className="t-body  f6  mid-grey  dib  ph1">|</span>
+              <time className="t-body  f6  mid-grey  dib" dateTime={item.publishedAt}>{item.publishedAt.split('T')[0]}</time>
+            </div>
+            <Link className="link" to={`/article/${item.slug.current}`}>
+              <p className="t-title  bold  f6  f5-md  black  tac  pt1  pb2">{item.title}</p>
+            </Link>
+            <p className="t-body  f6  mid-grey  tac  rel">{item.description.substring(0, 150)}...</p>
+          </article>
+        ))}
       </section>
     );
   }
