a3e5d28fb1fbbd9d8400d0612fe9f46e443200f0
dan jones <dannyjones360@gmail.com>
2018-08-20 17:32:44 +0100

    Further progress

--- src/app/header/index.js
+++ src/app/header/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import classNames  from 'classnames';
 
-import logo from '../assets/logo.jpg';
+import logo from '../assets/logo-small.png';
 
 class Header extends Component {
   constructor(props) {
@@ -57,7 +57,7 @@ class Header extends Component {
       return false;
     };
 
-    const navMobileClass = classNames('nav__mobile  abs  bg-white  shadow4  z9  db  dn-md', {
+    const navMobileClass = classNames('nav__mobile  abs  bg-white  br  bc-light-grey  shadow2  z9  db  dn-md', {
       'is-active': this.state.mobileNavActive,
     });
 
@@ -66,13 +66,13 @@ class Header extends Component {
         <img className="header__logo  w3  abs" src={logo} alt="Rendah" />
 
         {/* Desktop */}
-        <header id="header-desktop" className="header  bb  bc-light-grey  shadow1  pb3  pt1  z9  dn  db-md">
+        <header id="header-desktop" className="header  bb  bc-light-grey  shadow2  pb3  pt1  z9  dn  db-md">
           <nav className="container-large  mla  mra  rel  nav__desktop">
-            <ul className="tac  mla  mra" id="links">
+            <ul className="tac  mla  mra  pt3" id="links">
               {links.map((link, index) => {
                 return (
                   <li className="di">
-                    <Link key={index} className={`di  t-title  f5  ttu  bold  ph2  link  ${isCurrent(link.to, current) ? 'mid-grey' : 'black'}`} to={link.to}>
+                    <Link key={index} className={`di  t-title  f5  bold  ph2  link  ${isCurrent(link.to, current) ? 'mid-grey' : 'black'}`} to={link.to}>
                       {link.text}
                     </Link>
                   </li>
@@ -83,7 +83,7 @@ class Header extends Component {
         </header>
 
         {/* Mobile */}
-        <header id="header-mobile" className="header  bb  bc-light-grey  shadow1  pb3  pt1  z9  db  dn-md">
+        <header id="header-mobile" className="header  bb  bc-light-grey  shadow2  pb3  pt1  z9  db  dn-md">
           <div onClick={this.toggleMobileNav} className="abs  header__menuOpen">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
               <path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z"/>
@@ -91,16 +91,17 @@ class Header extends Component {
           </div>
 
           <nav className={navMobileClass}>
+            <input className="nav__mobile__search  t-title  f5  light-grey  mt3  ml3  w-70  bb  bc-light-grey  pr3  pv2" type="search" placeholder="Search by keyword" />
             <div onClick={this.toggleMobileNav} className="abs  header__menuClose">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/>
+                <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 16.538l-4.592-4.548 4.546-4.587-1.416-1.403-4.545 4.589-4.588-4.543-1.405 1.405 4.593 4.552-4.547 4.592 1.405 1.405 4.555-4.596 4.591 4.55 1.403-1.416z"/>
               </svg>
             </div>
-            <ul className="tal  abs  nav__mobile__list" id="links">
+            <ul className="tal  abs  w-100  nav__mobile__list" id="links">
               {links.map((link, index) => {
                 return (
-                  <li className="db">
-                    <Link key={index} className={`db  t-title  f4  ttu  f5-md  bold  ph2  pb2  link  ${isCurrent(link.to, current) ? 'mid-grey' : 'black'}`} to={link.to}>
+                  <li className="db  pv3  bb  bc-light-grey">
+                    <Link key={index} className={`db  t-title  f4  f5-md  bold  ph3  link  ${isCurrent(link.to, current) ? 'mid-grey' : 'black'}`} to={link.to}>
                       {link.text}
                     </Link>
                   </li>
