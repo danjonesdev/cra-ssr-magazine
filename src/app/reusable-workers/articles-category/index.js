4a66609eb99f48e80d36161396119defcb59b901
dan jones <dannyjones360@gmail.com>
2018-08-24 08:11:52 +0100

    Moved category into its own worker

---
+++ src/app/reusable-workers/articles-category/index.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { frontloadConnect } from 'react-frontload';
+
+import ArticlesGrid from '../../components/articles-grid';
+
+import {
+  getCurrentSubject,
+  removeCurrentSubjects,
+} from '../../../modules/category';
+
+const frontload = async props =>
+  await props.getCurrentSubject(props.match.params.id);
+
+class ArticlesCategory extends Component {
+  componentWillUnmount() {
+    this.props.removeCurrentSubjects();
+  }
+
+  shouldComponentUpdate(nextProps) {
+    if (nextProps.match.params.id !== this.props.match.params.id) {
+      this.props.getCurrentSubject(nextProps.match.params.id);
+    }
+    return true;
+  }
+
+  render() {
+    const category = this.props.currentProfiles;
+
+    if (category._id) {
+      return (
+        <div>
+          <ArticlesGrid items={category.posts} type="grid" padding="pt4  pb2" />
+        </div>
+      );
+    }
+    return false;
+  }
+}
+
+const mapStateToProps = state => ({
+  currentProfiles: state.category.currentProfiles,
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      getCurrentSubject,
+      removeCurrentSubjects,
+    },
+    dispatch
+  );
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(
+  frontloadConnect(frontload, {
+    onMount: true,
+    onUpdate: false,
+  })(ArticlesCategory)
+);
--- src/app/routes/category/index.js
+++ src/app/routes/category/index.js
@@ -1,78 +1,32 @@
 import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { frontloadConnect } from 'react-frontload';
 
 import Page from '../../components/page';
 import AnimatedHeading from '../../elements/animated-heading';
-import ArticlesGrid from '../../components/articles-grid';
+// import ArticlesLatest from '../../reusable-workers/articles-latest';
+import ArticlesCategory from '../../reusable-workers/articles-category';
 
-import {
-  getCurrentSubject,
-  removeCurrentSubjects,
-} from '../../../modules/category';
-
-const frontload = async props =>
-  await props.getCurrentSubject(props.match.params.id);
-
-class Article extends Component {
-  componentWillUnmount() {
-    this.props.removeCurrentSubjects();
-  }
-
-  shouldComponentUpdate(nextProps) {
-    if (nextProps.match.params.id !== this.props.match.params.id) {
-      this.props.getCurrentSubject(nextProps.match.params.id);
-    }
-    return true;
-  }
-
+class Homepage extends Component {
   render() {
-    const category = this.props.currentProfiles;
+    return (
+      <Page id="home" title="Home">
+        <AnimatedHeading
+          headingType="h1"
+          title="Category"
+          styles="t-title  ttu  bold  f5"
+          container
+          padding="pt4  mt2"
+        />
 
-    if (category._id) {
-      return (
-        <Page
-          id={category.slug.current}
-          title={category.title}
-          description={category.description}
-          image="{item.mainImage}"
-        >
-          <AnimatedHeading
-            headingType="h1"
-            title={category.title}
-            styles="t-title  ttu  bold  f5"
-            container
-            padding="pt4  mt2"
-          />
-
-          <ArticlesGrid items={category.posts} type="grid" padding="pt4  pb2" />
-        </Page>
-      );
-    }
-    return false;
+      <ArticlesCategory
+          {...this.props}
+          limitFrom={0}
+          limitTo={24}
+          type="grid"
+          padding="pt2  pb3"
+        />
+      </Page>
+    );
   }
 }
 
-const mapStateToProps = state => ({
-  currentProfiles: state.category.currentProfiles,
-});
-
-const mapDispatchToProps = dispatch =>
-  bindActionCreators(
-    {
-      getCurrentSubject,
-      removeCurrentSubjects,
-    },
-    dispatch
-  );
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(
-  frontloadConnect(frontload, {
-    onMount: true,
-    onUpdate: false,
-  })(Article)
-);
+export default Homepage;
